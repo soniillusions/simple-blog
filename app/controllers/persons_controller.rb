8a1436fdb9c67a57a786edbb591963aa723a77a0
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-11-25 16:36:54 +0300

    layout, navbar fix

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,4 +1,28 @@
 class PersonsController < ApplicationController
+  before_action :set_user!, only: %i[edit update]
+
   def profile
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      flash[:success] = 'Profile updated'
+      redirect_to persons_profile_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_user!
+    @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
