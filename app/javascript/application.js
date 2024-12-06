37781ebc94bf9b6c385b47d16d9b61ae2501b89c
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-12-06 23:31:13 +0300

    some layout fix, add left card nav

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Entry point for the build script in your package.json
 import * as bootstrap from "bootstrap"
+import Rails from "@rails/ujs";
 
 import 'bootstrap/js/dist/collapse'
 
-import Rails from "@rails/ujs";
-Rails.start();
+import Lightbox from 'bs5-lightbox';
+document.querySelectorAll('.my-lightbox-toggle').forEach(el => el.addEventListener('click', Lightbox.initialize));
 
+Rails.start();
