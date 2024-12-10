416c1579a9ce574c8d25d06ee63531602b9491d4
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-12-10 20:38:49 +0300

    add RSpec, add spec for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      user = User.new(email: 'test_email@gmail.com', password: '123321', password_confirmation: '123321')
+      expect(user).to be_valid
+    end
+
+    it 'is not valid with different passwords' do
+      user = User.new(email: 'test_email@gmail.com', password: '123321', password_confirmation: '111111')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with invalid Email' do
+      user = User.new(email: 'test_email.com', password: '123321', password_confirmation: '123321')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without an Email' do
+      user = User.new(email: nil)
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without password' do
+      user = User.new(email: 'test_email@gmail.com')
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe 'callbacks' do
+    it 'generates random username before create' do
+      user = User.create(email: 'test_email@gmail.com', password: 'password', password_confirmation: 'password')
+      expect(user.username).to match(/^user#[a-f0-9]{12}$/) # Регулярное выражение для проверки формата пароля
+    end
+  end
+
+  describe '#guest?' do
+    it 'returns false' do
+      user = User.new(email: 'test_email@gmail.com')
+      expect(user.guest?).to eq(false)
+    end
+  end
+end
