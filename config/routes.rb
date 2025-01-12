b3571212920479773e31de595426742c417b433d
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-01-12 18:13:38 +0300

    update Registration#edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
 
   resources :users, only: [:edit, :update, :show], param: :username
+
+  resource :password_reset, only: %i[new create edit update]
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
